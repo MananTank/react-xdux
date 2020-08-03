ebcd0f296413b68634116243ed1678cb5e3d0f10
Manan Tank <manantankm@gmail.com>
2020-08-03 22:24:08 +0530

    removed classes, new Hooks API - useStatics, useStore

--- checkers.js
+++ checkers.js
@@ -1,30 +1,14 @@
-const INVALID_DEP_ERROR = (dep, compName) => {
+const INVALID_SLICE_NAME = (dep, compName) => {
 	throw new Error(
-		`Invalid Dependency: "${dep}" provided for "${compName}" Component\n` +
-			`No such data item exists in state of store.`
-	);
-};
-
-const PROP_NAME_COLLISION = (dep, compName) => {
-	throw new Error(
-		`Prop Name Collision : "${dep}" is given as a prop to ${compName} component from both store and its parent component\n\n` +
-			'Change the prop name in parent to fix the collision\n\n'
+		`Asking for invalid slice "${dep}" in the useStore hook in <${compName}/> \n` +
+			`No such slice exists in the store.`
 	);
 };
 
 export const check_deps = (state, deps, compName) => {
 	if (deps && deps.length) {
 		for (const dep of deps) {
-			if (!(dep in state)) INVALID_DEP_ERROR(dep, compName);
+			if (!(dep in state)) INVALID_SLICE_NAME(dep, compName);
 		}
-	}
-};
-
-export const check_prop_collision = (deps, props, compName) => {
-	if (!props || !deps) return;
-	let propsArr = Object.keys(props);
-
-	for (const dep of deps) {
-		if (propsArr.includes(dep)) PROP_NAME_COLLISION(dep, compName);
 	}
 };
--- index.js
+++ index.js
@@ -1,53 +1,47 @@
-import React from 'react';
-import { check_deps, check_prop_collision } from './checkers';
+import React, { useContext, useEffect, useState } from 'react';
+import { check_deps } from './checkers';
+
+function getProps(store, deps, componentName) {
+	const props = {
+		dispatch: (...x) => store.dispatch(...x, componentName),
+	};
+
+	if (deps) for (const dep of deps) props[dep] = store.state[dep];
+	return props;
+}
 
 let StoreContext = React.createContext();
 
-// -----------------------------------------
-class Provider extends React.Component {
-	render() {
-		return React.createElement(
-			StoreContext.Provider,
-			{ value: { store: this.props.store } },
-			this.props.children
-		);
-	}
-}
+export const Provider = ({ children, store }) =>
+	React.createElement(StoreContext.Provider, { value: { store } }, children);
 
-// -----------------------------------------
-const connect = (Comp, deps) => {
-	const hasDeps = deps && deps.length;
+export const useStore = deps => {
+	const { store } = useContext(StoreContext);
+	check_deps(store.state, deps, useStore.caller.name);
+	const [props, setProps] = useState(getProps(store, deps, useStore.caller.name));
 
-	return class ConnectedComponent extends React.Component {
-		static contextType = StoreContext;
-
-		componentDidMount() {
-			check_deps(this.context.store.state, deps, Comp.name);
-			check_prop_collision(deps, this.props, Comp.name);
-
-			if (hasDeps) {
-				const listener = key => {
-					if (deps.includes(key)) this.forceUpdate();
-				};
-
-				this.unsubscribe = this.context.store.subscribe(listener);
+	useEffect(() => {
+		const listener = info => {
+			if (deps) {
+				let shouldUpdate = false;
+				for (const dep of deps) {
+					if (dep in info.updatedSlices) {
+						shouldUpdate = true;
+						break;
+					}
+				}
+				const compName = () => useStore.caller.name;
+				if (shouldUpdate) setProps(getProps(store, deps, compName));
 			}
-		}
+		};
 
-		componentWillUnmount() {
-			if (hasDeps) this.unsubscribe();
-		}
+		return store.subscribe(listener); // return cleanup
+	}, []);
 
-		render() {
-			const props = {
-				...this.props,
-				dispatch: (v, ad) => this.context.store.dispatch(v, ad, Comp.name),
-				_dispatch: (x, y) => () => this.context.store.dispatch(x, y, Comp.name),
-			};
-			if (hasDeps) deps.forEach(d => (props[d] = this.context.store.state[d]));
-			return React.createElement(Comp, props);
-		}
-	};
+	return props;
 };
 
-export { Provider, connect };
+export const useStatics = () => {
+	const { store } = useContext(StoreContext);
+	return store.statics;
+};
