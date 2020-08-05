707351dc81ec26504e9fec6428943aae0826d299
Manan Tank <manantankm@gmail.com>
2020-08-05 13:59:06 +0530

    useFullStore, eslint, minor refactoring

--- helpers.js
+++ helpers.js
@@ -1,36 +1,39 @@
 const INVALID_SLICE_NAME = (dep, compName) => {
-	console.trace();
-	throw new Error(
-		`Asking for invalid slice "${dep}" in the useStore hook in <${compName}/> \n` +
-			`No such slice exists in the store.`
-	);
-};
-
-export const check_deps = (state, deps, compName) => {
-	if (deps && deps.length) {
-		for (const dep of deps) {
-			if (!(dep in state)) INVALID_SLICE_NAME(dep, compName);
-		}
-	}
-};
-
-export function whoCalledMe() {
-	try {
-		throw new Error();
-	} catch (e) {
-		try {
-			return e.stack.split('at ')[3].split(' ')[0];
-		} catch (e) {
-			return '';
-		}
-	}
+  throw new Error(
+    `Asking for invalid slice "${dep}" in the useStore hook in <${compName}/> \n` +
+      'No such slice exists in the store.'
+  )
 }
 
-export function getProps(store, deps, componentName) {
-	const props = {
-		dispatch: (...x) => store.dispatch(...x, componentName),
-	};
+export const checkDepsValidity = (state, deps, compName) => {
+  if (deps && deps.length) {
+    for (const dep of deps) {
+      if (!(dep in state)) INVALID_SLICE_NAME(dep, compName)
+    }
+  }
+}
 
-	if (deps) for (const dep of deps) props[dep] = store.state[dep];
-	return props;
+export function whoCalledMe () {
+  try {
+    throw new Error()
+  } catch (e) {
+    try {
+      return e.stack.split('at ')[3].split(' ')[0]
+    } catch (e2) {
+      return ''
+    }
+  }
+}
+
+export function getProps (store, deps, componentName) {
+  const props = {
+    dispatch: (...x) => store.dispatch(...x, componentName)
+  }
+
+  if (deps && deps.length) {
+    for (const dep of deps) {
+      props[dep] = store.state[dep]
+    }
+  }
+  return props
 }
--- index.js
+++ index.js
@@ -1,40 +1,47 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { check_deps, whoCalledMe, getProps } from './helpers';
+import React, { useContext, useEffect, useState } from 'react'
+import { checkDepsValidity, whoCalledMe, getProps } from './helpers'
 
-let StoreContext = React.createContext();
+const StoreContext = React.createContext()
 
+// eslint-disable-next-line react/prop-types
 export const Provider = ({ children, store }) =>
-	React.createElement(StoreContext.Provider, { value: { store } }, children);
+  React.createElement(StoreContext.Provider, { value: { store } }, children)
 
 export const useStore = deps => {
-	const { store } = useContext(StoreContext);
-	const componentName = whoCalledMe();
+  const { store } = useContext(StoreContext)
+  const componentName = whoCalledMe()
 
-	check_deps(store.state, deps, componentName);
-	const [props, setProps] = useState(getProps(store, deps, componentName));
+  checkDepsValidity(store.state, deps, componentName)
+  const [props, setProps] = useState(getProps(store, deps, componentName))
 
-	useEffect(() => {
-		const listener = info => {
-			if (deps) {
-				let shouldUpdate = false;
-				for (const dep of deps) {
-					if (dep in info.updatedSlices) {
-						shouldUpdate = true;
-						break;
-					}
-				}
+  useEffect(() => {
+    const listener = info => {
+      if (deps) {
+        let shouldUpdate = false
+        for (const dep of deps) {
+          if (dep in info.updatedSlices) {
+            shouldUpdate = true
+            break
+          }
+        }
 
-				if (shouldUpdate) setProps(getProps(store, deps, componentName));
-			}
-		};
+        if (shouldUpdate) setProps(getProps(store, deps, componentName))
+      }
+    }
 
-		return store.subscribe(listener); // return cleanup
+    return store.subscribe(listener) // return cleanup
+    // eslint-disable-next-line
 	}, []);
 
-	return props;
-};
+  return props
+}
 
 export const useStatics = () => {
-	const { store } = useContext(StoreContext);
-	return store.statics;
-};
+  const { store } = useContext(StoreContext)
+  return store.statics
+}
+
+export const useFullStore = () => {
+  const { store } = useContext(StoreContext)
+  return store
+}
